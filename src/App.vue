dc5033ae84706025276eb29eeaf2142e0fab33ab
Christiaan Quyn <christianq010@gmail.com>
2018-09-25 13:56:25 +0530

    Add number navbar with fonts and styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"></router-link>
+      <!-- <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  @import '@/scss/base.scss';
+@charset "UTF-8";
+
+// font imports
+@font-face {
+  font-family: AvenirNextReg;
+  src: url("./assets/fonts/AvenirNext-Regular.ttf") format("truetype");
+}
+@font-face {
+  font-family: AvenirNextBold;
+  src: url("./assets/fonts/AvenirNext-Bold.ttf") format("truetype");
+}
+@font-face {
+  font-family: HelveticaNeueUltraLight;
+  src: url("./assets/fonts/HelveticaUltraLt.ttf") format("typetype");
+}
+@font-face {
+  font-family: HelveticaNeueLight;
+  src: url("./assets/fonts/HelveticaNeue-Light.otf") format("opentype");
+}
+@font-face {
+  font-family: HelveticaNeue;
+  src: url("./assets/fonts/HELVETICA.TTF") format("truetype");
+}
+@font-face {
+  font-family: HelveticaNeueBold;
+  src: url("./assets/fonts/HELVETICA-BOLD.TTF") format("truetype");
+}
+
+@import "@/scss/base.scss";
+html {
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body {
+  font-family: "AvenirNextReg", "HelveticaNeue", Arial, sans-serif;
+  margin: 0px;
+  padding: 0px;
+}
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="container">
+
+        <!-- Number tab -->
+        <div class="number-tab">
+            <!-- Number tab wrapper -->
+            <div class="number-tab-wrapper">
+                <!-- Number tab Inner -->
+                <div class="number-tab-inner">
+                    <!-- Number Tab Icon -->
+                    <div class="number-tab-icn-box">
+                        <div class="number-tab-icn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M16 11c0 2.209-1.791 4-4 4s-4-1.791-4-4v-7c0-2.209 1.791-4 4-4s4 1.791 4 4v7zm4-2v2c0 4.418-3.582 8-8 8s-8-3.582-8-8v-2h2v2c0 3.309 2.691 6 6 6s6-2.691 6-6v-2h2zm-7 13v-2h-2v2h-4v2h10v-2h-4z"/>
+                            </svg>
+                        </div>
+                        <!-- Language selector -->
+                        <div class="number-tab-lang-box">
+                            <div class="number-tab-lang">
+                                English
+                            </div>
+                        </div>
+                        <!-- /end of Language selector -->
+                        <div class="number-tab-dropdown">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24">
+                                <path d="M12 21l-12-18h24z"/>
+                            </svg>
+                        </div>
+                        <div class="number-tab-link">
+                            |
+                        </div>
+                        <div class="number-tab-num-box">
+                            <div class="number-tab-num-txt">
+                                Sales: +61 2 8046 6514
+                            </div>
+                        </div>
+                        <div class="number-tab-dropdown">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24">
+                                <path d="M12 21l-12-18h24z"/>
+                            </svg>
+                        </div>
+                        <!-- /end of Number tab number box -->
+                    </div>
+                    <!-- /end of Number Tab Icon -->
+                    <!-- Number tab number box -->
+                    <!-- Number tab Button -->
+                    <div class="number-tab-btn-box">
+
+                    </div>
+                    <!-- /end of Number tab Button -->
+                </div>
+                <!-- /end of Number tab Inner -->
+            </div>
+            <!-- /end of Number tab wrapper -->
+        </div>
+        <!-- /end of Number tab -->
+
+        <!-- Navbar elements -->
+        <div class="navbar">
+
+            <!-- Wrapper for all nav content -->
+            <div class="nav-wrapper">
+
+                <!-- Hamburger menu -->
+                <div class="menu-icn-wrapper">
+                    <i class="fas fa-align-justify menu-icn"></i>
+                </div>
+                <!-- /end of Hamburger menu -->
+
+                <!-- Our main Huff Post logo -->
+                <div class="logo">
+                    <!-- <img src="img/HuffPostHero.svg"> -->
+                    <h1>Logo</h1>
+                </div>
+                <!-- /end of main logo -->
+
+                <!-- Language Selection -->
+                <div class="language-selection">
+                    <span class="language-editions-text">Right Side</span>
+                </div>
+                <!-- /end of Language Selection -->
+
+            </div>
+            <!-- /end of Wrapper for all nav content -->
+
+        </div>
+        <!-- /end of Navbar -->
+
+        <!-- Main Content Section -->
+
+        <!-- Main Content Wrapper with image-->
+        <div class="main-content-wrapper" style="background-image: url('img/main-bg.jpeg');">
+        </div>
+        <!-- Main Content Wrapper -->
+
+        <!-- /end of Main Content Section -->
+
+
+        <!-- Content Section 1 -->
+        <div class="sec-1">Content Section 1</div>
+        <!-- /end of Content Section 1 -->
+
+        <!-- Content Section 2 (center) -->
+        <div class="sec-2">Content Section 2</div>
+        <!-- /end of Content Section 2 (center) -->
+
+        <!-- Content Section 3 (right) -->
+        <div class="sec-3">Content Section 3</div>
+        <!-- /end of Content Section 3 (right) -->
+
+
+        <!-- Stats Content Section -->
+        <!-- Table Stats -->
+        <div class="stats-1"> Stats big</div>
+        <!-- /end of Table Stats -->
+
+        <!-- Stats Customers -->
+        <div class="stats-2"> Stats left</div>
+        <!-- /end of Customers -->
+
+        <!-- Stats Start -->
+        <div class="stats-3">Stats right</div>
+        <!-- /end of Stats Start-->
+        
+        <!-- Footer -->
+        <div class="footer">Footer</div>
+        <!-- /end of Footer -->
+
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
