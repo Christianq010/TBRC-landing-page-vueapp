6bddfaf3f5546a0b8b3b13ebb1cbb9b0349f6ab5
Christiaan Quyn <christianq010@gmail.com>
2018-09-26 14:47:04 +0530

    Clean up unused component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <home-page />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import HomePage from '@/components/HomePage.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
     HomePage
   }
 }
